7be57ba68780760b32c814678472110532c37542
Tevin <motevin@gmail.com>
2018-04-07 15:54:29 +0300

    geoff needs helpers

--- badges-api/score/times.js
+++ badges-api/score/times.js
@@ -1,7 +1,7 @@
 'use strict';
-import * as haversine from 'haversine';
+require('./helpers/distance');
 
-export async function scoreTime(legs, user) {
+module.exports = async function scoreTime(legs, user) {
     for (leg in legs) {
         if (leg.legType === 'matatu') {
             const {wimtLineID, waypoints} = leg;
@@ -14,25 +14,4 @@ export async function scoreTime(legs, user) {
             
         }
     }
-
-}
-
-/**
- * Gets total distance between list of points
- * @param {Array} points List of points as an array of arrays, where each array is of the form [lat, long]
- */
-function getTotalDistance(points) {
-    let totalDistanceSoFar = 0;
-    for(let i = 1; i < points.length; i++) {
-        const start = {
-            latitude: points[i-1][0], 
-            longitude: points[i-1][1]
-        };
-        const end = {
-            latitude: points[i][0], 
-            longitude: points[i][1]
-        }
-        totalDistanceSoFar += haversine(start, end, {unit: 'meter'})
-    }
-    return totalDistanceSoFar;
 }
