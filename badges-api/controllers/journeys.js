f94b500efc618189731f598e7b6d5d395dea9f88
Geoffrey Mureithi <mureithinjuguna@gmail.com>
2018-04-07 21:51:54 +0300

    Added Models and Logic to store

--- badges-api/controllers/journeys.js
+++ badges-api/controllers/journeys.js
@@ -1,5 +1,7 @@
 const Journey = require('../models/journey')
 const TimesLeaderboard = require('../models/times');
+const CostLeaderboard = require('../models/costs');
+const EmissionsLeaderboard = require('../models/emissions');
 exports.index = (req, res) => {
   res.json({'message': 'Welcome'})
 }
@@ -7,18 +9,32 @@ const scoreGenerator = require('../score')
 exports.save = async (req, res) => {
   const journey = await Journey.create(req.body);
   const scores = scoreGenerator(journey);
-
+  const defaults = {
+    user: journey.user,
+    journey: journey._id
+  }
+  res.json({journey, scores});
   // save scores for times leaderboard
   const {score} = scores.find(s => s.type === 'time');
   score.map(async (leg) => {
     const toSave = {
-      user: journey.user,
-      journey: journey._id,
+      ...defaults,
       line: leg.wimtLineID,
       score: leg.timeScore
     }
     await TimesLeaderboard.create(toSave);
   });
 
-  res.json({journey, scores});
+  const costScore = scores.find(s => s.type === 'cost');
+  if(costScore)
+  await CostLeaderboard.create({
+    ...defaults,
+    score: costScore.score
+  })
+  const emissionsScore = scores.find(s => s.type === 'emissions');
+  if(emissionsScore)
+  await EmissionsLeaderboard.create({
+    ...defaults,
+    score: emissionsScore.score
+  })
 }
---
+++ badges-api/models/emissions.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const emissionsLeaderboardSchema = new Schema({
+    user: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    },
+    journey: {
+        type: Schema.ObjectId,
+        ref: 'Journey'
+    },
+    score: {
+        type: Number,
+        default: 0
+    }
+});
+
+mongoose.model('EmissionsLeaderboard', emissionsLeaderboardSchema)
