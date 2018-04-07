472929e4ab2e3b64bf57362c78b6988141b3d57a
Geoffrey Mureithi <mureithinjuguna@gmail.com>
2018-04-07 15:58:26 +0300

    Added test journeys api

--- badges-api/config/routes.js
+++ badges-api/config/routes.js
@@ -1,5 +1,8 @@
+const journeysApi = require('../controllers/journeys')
 module.exports = (app) => {
   app.get('/', (req, res) => {
     res.send('Welcome to BabasMatatu')
   })
+  app.get('/journeys', journeysApi.index)
+  app.post('/journeys', journeysApi.save)
 };
