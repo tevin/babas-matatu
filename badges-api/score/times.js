169e218dc523e8329692f1f4aa259b1fecbb9d54
Tevin <motevin@gmail.com>
2018-04-07 15:08:11 +0300

    time scoring skeleton

--- badges-api/score/times.js
+++ badges-api/score/times.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// test
+export async function scoreTime(legs, user) {
+    for (leg in legs) {
+        if (leg.legType === 'matatu') {
+            const {wimtLineID, waypoints} = leg;
+            const stopsTraversed = waypoints.length;
+            
+            // Calculate Total Distance Between Waypoints
+            
+        }
+    }
+
+}
